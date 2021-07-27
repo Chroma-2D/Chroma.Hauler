fefb1458167423d1367e8c63db8947301d06c824
vddCore <vddcore@outlook.com>
2021-07-27 13:46:14 +0200

    cleanup

--- Chroma.Hauler.Test/Program.cs
+++ Chroma.Hauler.Test/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
 namespace Chroma.Hauler.Test
 {
@@ -10,7 +11,7 @@ namespace Chroma.Hauler.Test
             var vfs = new VirtualFileSystem();
             vfs.Mount("/home/vdd/Documents/test.zip", "/blah");
             vfs.Mount("/home/vdd/Documents/test2.zip", "/blah");
-
+            vfs.Root.PutFile<CountFile>("/blah/counter");
 
             using (var fs = vfs.Root.OpenFile("/blah/test/test.txt"))
             using (var sr = new StreamReader(fs))
@@ -23,6 +24,24 @@ namespace Chroma.Hauler.Test
             {
                 Console.WriteLine(sr.ReadToEnd());
             }
+
+            for (var i = 0; i < 10; i++)
+            {
+                var value = BitConverter.ToInt32(vfs.Root.GetFile("/blah/counter").Read());
+                Console.WriteLine(value);
+            }
+        }
+
+        private string EnumerateDirectory(VfsDirectory directory)
+        {
+            var sb = new StringBuilder();
+
+            foreach (var dir in directory.GetDirectories())
+            {
+                sb.Append()
+            }
+            
+            return sb.ToString();
         }
     }
 }
